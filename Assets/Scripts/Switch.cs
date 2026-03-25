1979f203b5bd2fa59971fda902dbb3f79acefa09
EaloEafit <lcealoo@eafit.edu.co>
2026-03-25 06:57:33 -0500

    Switch

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -2,10 +2,30 @@ using UnityEngine;
 
 public class Switch : MonoBehaviour
 {
+    public int ClassSelection = 2;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        string className;
+
+        switch (ClassSelection)
+        {
+            case 1:
+                className = "Warrior";
+                break;
+            case 2:
+                className = "Wizzard";
+                break;
+            case 3:
+                className = "Archer";
+                break;
+            default: 
+                className = "Invalid class";
+                break;
+        }
+
+        Debug.Log ("You selected the class: " + className);
     }
 
     // Update is called once per frame
